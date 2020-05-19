8471c50f461200a71da54ad76b276f775c2a1b85
yoeko <lyk1719@gmail.com>
2020-05-19 17:34:18 +0200

    first commit

---
+++ monappli/src/main/java/com/objis/mavenapps/App.java
@@ -0,0 +1,13 @@
+package com.objis.mavenapps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
